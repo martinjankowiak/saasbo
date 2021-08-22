2f7a09e8b3a624c2bb51f1ed0f28dd95706cc1b9
martin jankowiak <jankowiak@gmail.com>
2021-08-22 15:34:04 +0000

    add util.py

--- saasgp.py
+++ saasgp.py
@@ -11,26 +11,10 @@ from jax import jit, vmap
 from jax.scipy.linalg import cho_factor, cho_solve, solve_triangular
 from numpyro.diagnostics import summary
 from numpyro.infer import MCMC, NUTS
+from util import chunk_vmap
 
 root_five = math.sqrt(5.0)
 five_thirds = 5.0 / 3.0
-
-
-def get_chunks(L, chunk_size):
-    num_chunks = L // chunk_size
-    chunks = [jnp.arange(i * chunk_size, (i + 1) * chunk_size) for i in range(num_chunks)]
-    if L % chunk_size != 0:
-        chunks.append(np.arange(L - L % chunk_size, L))
-    return chunks
-
-
-# fun should return tuples of arrays
-def chunk_vmap(fun, array, chunk_size=4):
-    L = array[0].shape[0]
-    chunks = get_chunks(L, chunk_size)
-    results = [vmap(fun)(*tuple([a[chunk] for a in array])) for chunk in chunks]
-    num_results = len(results[0])
-    return tuple([jnp.concatenate([r[k] for r in results]) for k in range(num_results)])
 
 
 # compute diagonal component of kernel
---
+++ util.py
@@ -0,0 +1,15 @@
+def get_chunks(L, chunk_size):
+    num_chunks = L // chunk_size
+    chunks = [jnp.arange(i * chunk_size, (i + 1) * chunk_size) for i in range(num_chunks)]
+    if L % chunk_size != 0:
+        chunks.append(np.arange(L - L % chunk_size, L))
+    return chunks
+
+
+# fun should return tuples of arrays
+def chunk_vmap(fun, array, chunk_size=4):
+    L = array[0].shape[0]
+    chunks = get_chunks(L, chunk_size)
+    results = [vmap(fun)(*tuple([a[chunk] for a in array])) for chunk in chunks]
+    num_results = len(results[0])
+    return tuple([jnp.concatenate([r[k] for r in results]) for k in range(num_results)])
